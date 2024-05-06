85e0aad5799347e1353aabedd6d8652c8a91e11f
Krishnam Shastri <krishnamshastri07@gmail.com>
2024-05-06 10:09:36 +0530

    Initialized repoitry and made the sign up page and the home page for the grocery app

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  #root to: "home#index"
+
+  root 'home#index'
+  resources :users, only: [:new, :create]  # Assuming signup functionality
+  get '/login' => 'sessions#new', as: 'login'  # Example login route
+  get '/about_us' => 'pages#about', as: 'about_us'  # Example about us route
+
+end
